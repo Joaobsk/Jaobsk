5abde429223871ab531e1a1309deb1cee665d135
Joaobsk <joaogabrielsilvabnu@gmail.com>
2024-07-11 11:55:39 -0300

    Forca-Arquivos separados

---
+++ 2-forca/Imprime_cabecalho.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include"imprime_cabecalho.hpp"
+
+void imprime_cabecalho(){
+    std::cout << "*********************" << std::endl;
+    std::cout << "*** Jogo da Forca ***" << std::endl;
+    std::cout << "*********************" << std::endl;
+    std::cout << std::endl;
+}
